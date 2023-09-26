3fcdf19855f3609e9b2ed9bdc62c84379db25905
mauLovera <mauricioloveraedu@gmail.com>
2023-09-26 18:17:20 -0400

    feat: add color and emojis to output

--- weather/src/main.rs
+++ weather/src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use colored::Colorize;
 use dotenv;
 use serde::{Deserialize, Serialize};
 #[tokio::main]
@@ -10,14 +11,9 @@ async fn main() -> Result<(), reqwest::Error> {
     #[derive(Parser, Debug)]
     #[command(author, version, about, long_about = None)]
     struct Args {
-        /// Name of the person to greet
-        #[arg(long, default_value = "Mauricio")]
-        name: String,
-
         /// Name of the city to lookup
         #[arg(short, long, default_value = "Miami")]
         city: String,
-
     }
 
     #[derive(Debug, Serialize, Deserialize)]
@@ -38,7 +34,7 @@ async fn main() -> Result<(), reqwest::Error> {
     struct Main {
         temp: f32,
         feels_like: f32,
-        humidity: f32,
+        // humidity: f32,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
@@ -55,17 +51,39 @@ async fn main() -> Result<(), reqwest::Error> {
         "https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=imperial"
     );
 
-    let response: CurrentWeather = reqwest::Client::new()
-        .get(url)
-        .send()
-        .await?
-        .json()
-        .await?;
+    let response: CurrentWeather = reqwest::Client::new().get(url).send().await?.json().await?;
+    let feels_like_emoji: &str;
 
-    println!(
-        "Hello {}! The temperature in {} is {}°f, but it feels like {}°f",
-        args.name, response.name, response.main.temp as i32, response.main.feels_like as i32
+    let is_hot_outside: bool = response.main.feels_like >= 80.0;
+    let is_nice_outside: bool = response.main.feels_like < 80.0 && response.main.feels_like >= 60.0;
+
+    if is_hot_outside {
+        feels_like_emoji = "🥵🔥";
+    } else if is_nice_outside {
+        feels_like_emoji = "😎🔆";
+    } else {
+        feels_like_emoji = "🥶🧊";
+    }
+
+    let output: String = format!("{} The temperature in {} is {}°f, but it feels like {}°f {}",
+        feels_like_emoji,
+        response.name,
+        response.main.temp as i32,
+        response.main.feels_like as i32,
+        feels_like_emoji
     );
+
+    let colorized_output: colored::ColoredString = {
+        if is_hot_outside {
+            output.red()
+        } else if is_nice_outside {
+            output.yellow()
+        } else {
+            output.blue()
+        }
+    };
+
+    println!("{}", colorized_output);
 
     Ok(())
 }
