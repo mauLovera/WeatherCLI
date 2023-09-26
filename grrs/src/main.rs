f879c2206c44bbb3a880c1bd12a1f2dba58b6da9
mauLovera <mauricioloveraedu@gmail.com>
2023-09-26 16:13:38 -0400

    feat: add lookup by city name

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,15 +1,24 @@
+use clap::Parser;
 use dotenv;
 use serde::{Deserialize, Serialize};
 #[tokio::main]
 
-
 async fn main() -> Result<(), reqwest::Error> {
     dotenv::dotenv().ok();
     let api_key: String = dotenv::var("OPEN_WEATHER_API_KEY").unwrap();
-    
-    const LAT: f32 = 38.8895;
-    const LON: f32 = -77.0353;
-    
+
+    #[derive(Parser, Debug)]
+    #[command(author, version, about, long_about = None)]
+    struct Args {
+        /// Name of the person to greet
+        #[arg(long, default_value = "Mauricio")]
+        name: String,
+
+        #[arg(short, long, default_value = "Miami")]
+        city: String,
+
+    }
+
     #[derive(Debug, Serialize, Deserialize)]
     struct Coord {
         lat: f32,
@@ -31,7 +40,6 @@ async fn main() -> Result<(), reqwest::Error> {
         humidity: f32,
     }
 
-
     #[derive(Debug, Serialize, Deserialize)]
     struct CurrentWeather {
         coord: Coord,
@@ -40,13 +48,23 @@ async fn main() -> Result<(), reqwest::Error> {
         name: String,
     }
 
-    let url = format!(
-        "https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={api_key}&units=imperial"
+    let args: Args = Args::parse();
+    let city_name: String = args.city;
+    let url: String = format!(
+        "https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=imperial"
     );
 
-    let response: CurrentWeather = reqwest::Client::new().get(url).send().await?.json().await?;
+    let response: CurrentWeather = reqwest::Client::new()
+        .get(url)
+        .send()
+        .await?
+        .json()
+        .await?;
 
-    println!("{:#?}", response);
+    println!(
+        "Hello {}! The temperature in {} is {}°f, but it feels like {}°f",
+        args.name, response.name, response.main.temp as i32, response.main.feels_like as i32
+    );
 
     Ok(())
 }
